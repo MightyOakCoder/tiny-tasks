810a53fbcc86108c59a5cc00a6f6424f9b4b082c
beckyweeks14 <mauransnow@Weeks-MacBook-Pro.local>
2023-02-10 15:15:07 -0500

    adding .env and confirming connects to server

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
+mongoose.set("strictQuery", false)
+
 mongoose.connect(process.env.DATABASE_URL);
 
-const db = mongoose.connection;
+const db = mongoose.connection
 
 db.on('connected', function () {
   console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
